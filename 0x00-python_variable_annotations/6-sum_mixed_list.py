dab62679110185fad4346e191f21816d2c0d6fe5
mbmande <mbsmande@gmail.com>
2025-01-03 19:01:06 +0000

    mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""======================================
+"""
+
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """=============================="""
+    return float(sum(mxd_lst))
