c0c5de4b38e554ff9621487d96a1a956d7536abd
mbmande <mbsmande@gmail.com>
2025-01-03 19:20:45 +0000

    checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""==============================================================
+"""
+
+# jglgslgkdglgsgslfkspfsk
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """ =========================
+    """
+    zoomed_in: Tuple = tuple(
+        item for item in lst
+        for i in range(factor)
+    )
+    return zoomed_in
+
+
+array = (12, 72, 91)  # use tuple instead of list
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)  # use interger value for the factor
