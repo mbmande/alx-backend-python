7eeb8154452f597b3c95b31a78c1b6a6d6de6928
mbmande <mbsmande@gmail.com>
2025-01-03 19:16:04 +0000

    first

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""==========================================
+"""
+
+from typing import List, Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """=============================================
+    """
+
+    if lst:
+        return lst[0]
+    else:
+        return None
