8b1b2088c23b1fb1b91d2eeb4451de05070d2706
mbmande <mbsmande@gmail.com>
2025-01-03 18:58:40 +0000

    listing sums

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""-=======================================
+"""
+ 
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """============================
+    """
+    sum = 0.0
+
+    for num in input_list:
+        if isinstance(num, float):  # if type(num) is float:
+            sum += num
+
+    return sum
